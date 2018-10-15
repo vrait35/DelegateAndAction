6bb4a78a73ae88b2f62ab8d841157b5dba059f03
Темирбулат Ануар Жанабекович <@corp.itstep>
2018-10-15 18:28:38 +0600

    asd

---
+++ Delegate/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public delegate void MessengeHandler(string message);
+    public class Account
+    {
+       // private MessengeHandler handler;
+        public event MessengeHandler ActionEvent;
+       // public void RegisterDelegate(MessengeHandler handler)
+       // {
+       //     this.handler += handler;
+       // }
+       // public void UnRegisterDelegate(MessengeHandler handler)
+       // {
+       //     this.handler -= handler;
+       // }
+
+        public int Sum { get; private set; }
+       
+        
+        public void  Add(int sum)
+        {
+           
+            Sum += sum;
+            ActionEvent?.Invoke("вы добавили свой счет: " + sum);
+        }
+        public void Withdraw(int sum)
+        {
+            if (Sum >= sum)
+            {
+                Sum -= sum;
+                ActionEvent?.Invoke("вы сняли бабки");
+            }
+            else
+            {
+                ActionEvent?.Invoke("недостаточно средств");
+            }
+        }      
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Account account = new Account();
+            // MessengeHandler handler = new MessengeHandler(new Messenger().SendMessage);
+            account.ActionEvent += new Messenger().SendMessage;
+            account.RegisterDelegate();
+            account.Add(1000);            
+            account.Withdraw(500);            
+            Console.WriteLine(account.Sum);
+            
+        }
+       
+    }
+}
